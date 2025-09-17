9649a170f7de1e235296ef991095e0a2a343b456
mariosstavr <stavropoulos_marios@yahoo.gr>
2025-09-17 16:34:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -288,5 +288,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 7020;
+const PORT = process.env.PORT || 6050;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://localhost:${PORT}`));
